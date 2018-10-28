06e539b6bfd38f3bc4efc9eb0432a9bd89d3bf05
Batuhan <batuhan.undar01@gmail.com>
2018-10-28 20:22:41 +0300

    -some performance changes -added weighted rating calculation -added algorithm score

--- KNN_Model.py
+++ KNN_Model.py
@@ -4,40 +4,85 @@ from numpy array
 
 """
 import numpy as np
+import cupy as cp
 
 
 class KNN:
     """ TODO: some more extensive testing required, somehow some datas are accurate while some other is not,
         One idea is to add 1 to all rating data so there is a distinction between dislike and no vote"""
+    _MAX_WORKER = 4
+
     def __init__(self):
-        pass
+        self.K = 1
+        self.score = 0.0
+        self.predict = []
+
+    """ Calculating algorithm score based on predicted array """
+    @staticmethod
+    def calc_score(arr, pred):
+        sum_score = 0
+        indices = arr.nonzero()
+        for i in indices[0]:
+            sum_score += abs(float(arr[i] - pred[i]))
+        return sum_score / len(indices[0]) if len(indices[0]) != 0 else 0.0
 
     @staticmethod
-    def det(arr):
-        data = np.sqrt(np.sum(np.square(arr)))
-        return data if data != 0 else 0.000001
+    def calc_prediction(arr, data, distances, nonzero=True):
+        indices = arr.nonzero()
+        simsum = sum(i for _, i in distances)
+        base = (sum([arr + (data[i[0]] * i[1]) for i in distances]) / simsum if simsum != 0 else arr)
+        for i in indices[0]:
+            base[i] = 0.0
+            counter = 0
+            for ind, sim in distances:
+                if data[ind][i] == 0:
+                    continue
+                base[i] += (data[ind][i] * sim)
+                counter += sim
+            base[i] = base[i] / counter if counter != 0 else base[i]
+        return base
 
-    # TODO: if possible change this cosine similarity function to adjusted one
-    @staticmethod
-    def sim(arr1, arr2):
-        return (np.dot(arr1, arr2)) / (np.linalg.norm(arr1) * np.linalg.norm(arr2))
-        # return (np.dot(arr1, np.transpose(arr2))) / (self.det(arr1) * self.det(arr2))
+    def sparse_inner(self, arr1, arr2):
+        indices = set(arr1.nonzero()[0])
+        indices.update(arr2.nonzero()[0])
+        sum = 0
+        for i in indices:
+            sum += (arr1[i] * arr2[i])
+
+        return sum
+
+    # TODO: add other similarity functions too
+    def sim(self, arr1, arr2):
+        # dp = np.dot(arr1, arr2)
+        dp = self.sparse_inner(arr1, arr2)
+        return dp / (np.linalg.norm(arr1) * np.linalg.norm(arr2)) if dp != 0 else 0.0
 
     def all_similarities(self, data, target):
         out = []
+
         for d in range(data.shape[0]):
-            out.append((d, self.sim(data[d], target)))
+            out.append((d, cp.asnumpy(self.sim(data[d], target))))
+
         y = sorted(out, key=lambda arr: arr[1], reverse=True)
         return y
 
-    def calculate_nearest_n(self, k, data, target):
-        """ TODO: add weight """
-        similarities = self.all_similarities(data, target)[:k]
-        out = np.zeros(data.shape[1])
-        for i in similarities:
-            out += data[i[0]]
+    """ 
+    Weighted neares n calculator, weights are not distance based but similarity based
+    Also calculates score based on predicted values
+        
+    """
+    def calculate_nearest_n(self, data, target):
+        # calculate similarities and slice first K
+        similarities = self.all_similarities(data, target)[:self.K]
 
-        return out / k
+        # calculate the predicted score with weight as similarity
+        # if nonzero flag is set to false users with no vote is also included as lowest score
+        pred = self.calc_prediction(target, data, similarities)
+        self.score += self.calc_score(target, pred)
+
+        return pred
 
     def fit(self, k, data, target):
-        return np.array([self.calculate_nearest_n(k, data, t) for t in target])
+        self.K = k
+        self.predict = np.array([self.calculate_nearest_n(data, t) for t in target])
+        self.score /= len(target)
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from KNN_Model import KNN
 from data_parser import CSVParser
 import numpy as np
+import cupy as cp
+import random as rd
 
 if __name__ == "__main__":
     model = KNN()
 
-    file_arr = ["data/BX-Users.csv", "data/BX-Book-Ratings-Train.csv", "data/BX-Books.csv"]
+    file_arr = ["data/BX-Users.csv", "data/BX-Book-Ratings-Train.csv", "data/BX-Books.csv",
+                "data/Test/Test-User_Rating300.csv"]
     data_arr = [CSVParser(f) for f in file_arr]
 
     data_arr[0].filter('Location', ", usa|, canada")
+
+    test_in = CSVParser(file_arr[3])
+    test_in.merge(data_arr[2], "ISBN")
+    t_arr = test_in.DF[['ISBN', 'User-ID', 'Book-Rating']].values
 
     merged = data_arr[1]
     merged.merge(data_arr[0], "User-ID")
@@ -17,12 +24,25 @@ if __name__ == "__main__":
     index1 = merged.indexify('ISBN')
     index2 = merged.indexify('User-ID')
 
+    tindex = test_in.indexify('User-ID')
+
     m_arr = merged.DF[['ISBN', 'User-ID', 'Book-Rating']].values
     n_arr = np.zeros((len(index2), len(index1)))
-    for row in m_arr:
-        n_arr[index2[row[1]]][index1[row[0]]] = (row[2] + 1)
+    tt_arr = np.zeros((len(tindex), len(index1)))
+    for row in t_arr:
+        try:
+            tt_arr[tindex[row[1]]][index1[row[0]]] = float(row[2] + 1)
+        except KeyError:
+            print("passing")
+            continue
 
-    data_arr, test_arr = n_arr[10:], n_arr[:10]
-    test = model.fit(3, data_arr, test_arr)
+    for row in m_arr:
+        n_arr[index2[row[1]]][index1[row[0]]] = float(row[2] + 1)
+
+    # data_arr, test_arr = n_arr[150:], n_arr[:150]
+    data_arr, test_arr = n_arr, tt_arr[:5]
+    model.fit(3, data_arr, test_arr)
+    predict = model.predict
+    print("Weighted score: ", model.score)
 
     print("end")
